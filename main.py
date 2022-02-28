fec18618c50edb06439805b093c1f42922f99e47
AronMeeus <aronmeeus@gmail.com>
2022-02-28 15:42:40 +0100

    scriptname

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def read_root():
 def read_script(script_name: str):
     # Run script via script-name
     # Error route for non existing script
-    subprocess.run(f"cd app/{script_name} && python3 {script_name}.py && cd ../..", shell=True)
+    subprocess.run(f"cd app/{script_name}/{script_name} && python3 {script_name}.py && cd ../..", shell=True)
 
     # json to tsv
     result = {}
